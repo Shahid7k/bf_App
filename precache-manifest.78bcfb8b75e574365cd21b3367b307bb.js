729e2f90358f5b782c548c3960fb11d6c6bffef9
Shahid7k <shahid71733@gmail.com>
2020-03-23 03:18:21 +0530

    Updates

---
+++ precache-manifest.78bcfb8b75e574365cd21b3367b307bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed4afee1754fc06b017d568e3647152",
+    "url": "/bf_App/index.html"
+  },
+  {
+    "revision": "3c794a87a69a302202c3",
+    "url": "/bf_App/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "068e627ad4fae132595a",
+    "url": "/bf_App/static/css/main.2d172194.chunk.css"
+  },
+  {
+    "revision": "3c794a87a69a302202c3",
+    "url": "/bf_App/static/js/2.1635b1e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bf_App/static/js/2.1635b1e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068e627ad4fae132595a",
+    "url": "/bf_App/static/js/main.7559cd70.chunk.js"
+  },
+  {
+    "revision": "bd5a069063354411c045",
+    "url": "/bf_App/static/js/runtime-main.d1ba7581.js"
+  }
+]);
