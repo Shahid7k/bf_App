4a1f4dfd07e4cc2a77c4812f0af628f1957f5c68
Shahid7k <shahid71733@gmail.com>
2020-03-23 15:54:30 +0530

    SearchBarEnabled

---
+++ build/precache-manifest.de8ffec728e2d4752879f542b207dc08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c249f07c35eb0b3804bbbb779813d861",
+    "url": "/bf_App/index.html"
+  },
+  {
+    "revision": "79a6f9ecf287da8cba01",
+    "url": "/bf_App/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "135e871af2d25c680e43",
+    "url": "/bf_App/static/css/main.2d172194.chunk.css"
+  },
+  {
+    "revision": "79a6f9ecf287da8cba01",
+    "url": "/bf_App/static/js/2.413cb2b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bf_App/static/js/2.413cb2b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135e871af2d25c680e43",
+    "url": "/bf_App/static/js/main.751b2423.chunk.js"
+  },
+  {
+    "revision": "bd5a069063354411c045",
+    "url": "/bf_App/static/js/runtime-main.d1ba7581.js"
+  }
+]);
