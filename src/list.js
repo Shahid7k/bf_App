4a4cd333c9328fbeb2828319a47447a1ca350f3d
Shahid7k <shahid71733@gmail.com>
2020-03-24 13:05:32 +0530

    DailyUpdate-24-3-20

--- src/list.js
+++ src/list.js
@@ -3,30 +3,30 @@ export const list = [
 	pic:"https://i.ibb.co/8B6HrXM/IMG-5983.jpg",
 	name : "Shahid",
 	no:"1285",
-	yesterday: " Basic React",
-	topics:"Alittle more React"
+	yesterday: "Alittle more React, Basic React",
+	topics:"Didn't decide Yet. :P"
 
 },
 {
 	pic:"https://i.ibb.co/NY97cDN/IMG-5897.jpg",
 	name : "Shraddha",
 	no:"1291",
-	yesterday: " Amit Sir Notes Revision",
-	topics:"Algo-Course : Week 2 Quiz"
+	yesterday: "Algo-Course : Week 2 Quiz, Amit Sir Notes Revision",
+	topics:"AECS Record writing"
 },
 {
 	pic:"https://i.ibb.co/QKskJ77/IMG-5957.jpg",
 	name : "Shritha",
 	no:"1292",
 	yesterday: " Coding & Decoding",
-	topics:"Emo.. Herpine reply e ivvadu."
+	topics:"Emo.. Heroine reply e ivvadu."
 },
 {
 	pic:"https://i.ibb.co/4VXkdww/IMG-5978.jpg?",
 	name : "Snake",
 	no:"12A7",
-	yesterday: " Cloud Computing lo edo Chapter",
-	topics:"EPAM"
+	yesterday: "EPAM, Cloud Computing lo edo Chapter",
+	topics:"Another EPAM Task"
 }
 ];
 
